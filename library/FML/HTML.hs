4fe3a27fe1a0960530d6bea73a5bac3fff66ba64
Fintan Halpenny <rossmanf@tcd.ie>
2017-09-02 19:07:43 +0100

    Some renaming. Added is* functions

--- library/FML/HTML.hs
+++ library/FML/HTML.hs
@@ -5,9 +5,11 @@
 
 module FML.HTML
     ( HTML
+    , Attribute (..)
     , openTag
     , textTag
     , commentTag
+    , emptyTag
     ) where
 
 import           Data.Fix
@@ -16,18 +18,38 @@ newtype Attribute str = Attribute (str, str)
   deriving (Show, Eq, Ord)
 
 data HTMLF str a =
-    TagOpen str [Attribute str] a
-  | TagText str
-  | TagComment str
-  deriving (Functor, Show, Eq, Ord, Foldable, Traversable)
+    OpenTag str [Attribute str] a
+  | TextTag str
+  | CommentTag str
+  | EmptyTag
+  deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
 
 type HTML str = Fix (HTMLF str)
 
 openTag :: str -> [Attribute str] -> HTML str -> HTML str
-openTag name attributes = Fix . TagOpen name attributes
+openTag name attributes = Fix . OpenTag name attributes
 
 textTag :: str -> HTML str
-textTag = Fix . TagText
+textTag = Fix . TextTag
 
 commentTag :: str -> HTML str
-commentTag = Fix . TagComment
+commentTag = Fix . CommentTag
+
+emptyTag :: HTML str
+emptyTag = Fix EmptyTag
+
+isOpenTag :: HTMLF str a -> Bool
+isOpenTag (OpenTag _ _) = True
+isOpenTag _             = False
+
+isTextTag  :: HTMLF str a -> Bool
+isTextTag (TextTag _) = True
+isTextTag _           = False
+
+isCommentTag :: HTMLF str a -> Bool
+isCommentTag (CommentTag _) = True
+isCommentTag _              = False
+
+isEmptyTag :: HTMLF str a -> Bool
+isEmptyTag EmptyTag = True
+isEmptyTag _        = False
